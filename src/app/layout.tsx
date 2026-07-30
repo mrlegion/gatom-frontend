2a7e1929c1624e4066ef3719aac2f9ee5c7ba521
Alexander (AlexDevCo) Borovskikh <borislav_god@mail.ru>
2026-07-30 21:14:12 +0500

    Инициализация проекта

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import { Geist, Geist_Mono } from 'next/font/google'
+
+import './globals.css'
+import { cn } from '@/utils'
+
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' })
+
+const fontMono = Geist_Mono({
+	subsets: ['latin'],
+	variable: '--font-mono'
+})
+
+export default function RootLayout({
+	children
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<html
+			lang='en'
+			suppressHydrationWarning
+			className={cn('antialiased', fontMono.variable, 'font-sans', geist.variable)}
+		>
+			<body>{children}</body>
+		</html>
+	)
+}
